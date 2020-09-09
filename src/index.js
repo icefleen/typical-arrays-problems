8349a29be679391d2a69c3f1c3d8805ab0a9f435
Даниил Яндыбаев <xramirezx@outlook.com>
2020-09-09 21:01:28 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!(array instanceof Array) || array.length === 0) return 0;
+  
+  let min_el = array[0];
+
+  for (let i = 1; i < array.length; i++) if (array[i] < min_el) min_el = array[i];
+
+  return min_el;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!(array instanceof Array) || array.length === 0) return 0;
+  
+  let max_el = array[0];
+
+  for (let i = 1; i < array.length; i++) if (array[i] > max_el) max_el = array[i];
+
+  return max_el;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!(array instanceof Array) || array.length === 0) return 0;
+
+  let sum = 0;
+
+  for (el of array) sum += el;
+
+  return sum / array.length;
 }
